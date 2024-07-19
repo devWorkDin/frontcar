791a1baab7764d35cfacd0ae067f4f1ce12a78af
Oussama DABACHIL <78686921+devWorkDin@users.noreply.github.com>
2024-07-19 16:55:45 +0800

    adding cdn layout

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
       <body className={inter.className}>{children}</body>
     </html>
   );

f25fa6403313e24ad65349539dc10efab7a15153
Oussama DABACHIL <78686921+devWorkDin@users.noreply.github.com>
2024-07-19 21:48:01 +0800

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,8 @@
-import { Inter } from "next/font/google";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@components/Footer";
 import Nav from "@components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <link rel="icon" type="image/x-icon" href="/" />
+      <link rel="icon" type="image/png" href="/faviconFC.png" />
       <body
         className={urbanist.className}
         style={{

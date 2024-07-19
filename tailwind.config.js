4c219935b72adb07506c767a8f9a2c0cb0e06bc2
Oussama DABACHIL <78686921+devWorkDin@users.noreply.github.com>
2024-07-19 16:51:13 +0800

    capitalize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Src/**/*.{html,js,svelte,ts}",
+    "./Pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./App/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
